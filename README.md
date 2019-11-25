c12eb34d05939c365f0fd6da7c376d5d1eb36dd6
ezekg <ezekg@yahoo.com>
2019-11-25 12:34:49 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Example Embedded License Key Data
+This is an example of embedding data within cryptographically signed license
+keys, and extracting said data out of the keys using your Keygen account's
+RSA public key. You can find your public key within
+[your account's settings page](https://app.keygen.sh/settings).
+
+Embedding data within cryptographically signed licenses can be used to implement
+robuest offline licensing models, as well as adding additional security to your
+licensing model. For example, you can embed privileges and expirys within the license's
+key which can be extracted offline. All that is needed to cryptographically verify
+a license and extract the embedded data is your account's public key.
+
+**For this example, the license's policy _must_ implement the `RSA_2048_PKCS1_PSS_SIGN`
+scheme. This example _does not_ cover any other schemes at this time.**
+
+## Running the example
+
+First up, add an environment variable containing your public key:
+```bash
+# Your Keygen account ID (find yours at https://app.keygen.sh/settings)
+export KEYGEN_ACCOUNT_ID="YOUR_KEYGEN_ACCOUNT_ID"
+
+# Keygen product token (don't share this!)
+export KEYGEN_PRODUCT_TOKEN="YOUR_KEYGEN_PRODUCT_TOKEN"
+
+# The Keygen policy to use when creating licenses for new users
+export KEYGEN_POLICY_ID="YOUR_KEYGEN_POLICY_ID"
+
+# Your Keygen account's public key (make sure it is *exact* - newlines and all)
+export KEYGEN_PUBLIC_KEY=$(printf %b \
+  '-----BEGIN PUBLIC KEY-----\n' \
+  'zdL8BgMFM7p7+FGEGuH1I0KBaMcB/RZZSUu4yTBMu0pJw2EWzr3CrOOiXQI3+6bA\n' \
+  # …
+  'efK41Ml6OwZB3tchqGmpuAsCEwEAaQ==\n' \
+  '-----END PUBLIC KEY-----')
+```
+
+You can either run each line above within your terminal session before
+starting the app, or you can add the above contents to your `~/.bashrc`
+file and then run `source ~/.bashrc` after saving the file.
+
+Next, install dependencies with [`yarn`](https://yarnpkg.comg):
+```
+yarn
+```
+
+## Configuring a license policy
+
+Visit [your dashboard](https://app.keygen.sh/policies) and create a new
+policy with the following `scheme` attribute:
+
+```javascript
+{
+  scheme: 'RSA_2048_PKCS1_PSS_SIGN'
+}
+```
+
+**This example _must_ use the `RSA_2048_PKCS1_PSS_SIGN` scheme.**
+
+## Generating a license key
+
+To generate a signed license key, run the `generate` script, passing in the some
+`data` to embed within the signed key:
+
+```
+yarn start generate --data '{"example":{"json":"embedded-data"}}'
+```
+
+## Generating a license key
+
+Then to verify the key, run the `verify` script, passing in the _entire_ signed
+`key` you received from the `generate` step above:
+
+```
+yarn start verify --key 'SOME_SIGNED.LICENSE_KEY_HERE'
+```
+
+The license key will be cryptographically verified and if valid, the key's embedded
+data will be extracted and displayed. Be sure to copy your public key correctly!
+Your keys will fail validation if it is copied incorrectly. You can find your
+public key in [your account's settings](https://app.keygen.sh/settings).
+
+## Questions?
+
+Reach out at [support@keygen.sh](mailto:support@keygen.sh) if you have any
+questions or concerns!
---
+++ index.js
@@ -0,0 +1,167 @@
+const {
+  KEYGEN_PUBLIC_KEY,
+  KEYGEN_PRODUCT_TOKEN,
+  KEYGEN_ACCOUNT_ID,
+  KEYGEN_POLICY_ID,
+} = process.env
+
+const fetch = require('node-fetch')
+const crypto = require('crypto')
+const chalk = require('chalk')
+
+async function verifyPolicyScheme(policyId) {
+  const res = await fetch(`https://api.keygen.sh/v1/accounts/${KEYGEN_ACCOUNT_ID}/policies/${policyId}`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${KEYGEN_PRODUCT_TOKEN}`,
+      'Accept': 'application/vnd.api+json'
+    }
+  })
+  const { data, errors } = await res.json()
+  if (errors) {
+    const msg = errors.map(e => `${res.status} ${e.title}: ${e.detail}`).join('\n')
+
+    throw new Error(`Failed to retrieve policy!\n${msg}`)
+  }
+
+  return data.attributes.scheme === 'RSA_2048_PKCS1_PSS_SIGN'
+}
+
+async function main() {
+  const [_yarn, _start, cmd, ...argv] = process.argv
+
+  // Sanity checks for environment vars
+  if (!KEYGEN_PRODUCT_TOKEN) {
+    throw new Error('Product API token is required')
+  }
+
+  if (!KEYGEN_ACCOUNT_ID) {
+    throw new Error('Account ID is required')
+  }
+
+  if (!KEYGEN_POLICY_ID) {
+    throw new Error('Policy ID is required')
+  }
+
+  if (!KEYGEN_PUBLIC_KEY) {
+    throw new Error('Public key is required')
+  }
+
+  if (!KEYGEN_PUBLIC_KEY.includes(`-----BEGIN PUBLIC KEY-----`) ||
+      !KEYGEN_PUBLIC_KEY.includes(`-----END PUBLIC KEY-----`)) {
+    throw new Error(`Public key is not valid:\n${KEYGEN_PUBLIC_KEY}`)
+  }
+
+  switch (cmd) {
+    case 'generate': {
+      let key = null
+
+      // Parse argument flags
+      argv.forEach((arg, i, argv) => {
+        switch (arg) {
+          case '--data':
+          case '-d':
+            key = argv[i + 1]
+            break
+        }
+      })
+
+      if (!key) {
+        throw new Error(`License key data is missing!`)
+      }
+
+      // Verify the policy is using the correct scheme
+      const ok = await verifyPolicyScheme(KEYGEN_POLICY_ID)
+      if (!ok) {
+        throw new Error(`Policy ${KEYGEN_POLICY_ID} is not using RSA_2048_PKCS1_PSS_SIGN scheme!`)
+      }
+
+      // Generate a new license key
+      const res = await fetch(`https://api.keygen.sh/v1/accounts/${KEYGEN_ACCOUNT_ID}/licenses`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${KEYGEN_PRODUCT_TOKEN}`,
+          'Content-Type': 'application/vnd.api+json',
+          'Accept': 'application/vnd.api+json'
+        },
+        body: JSON.stringify({
+          data: {
+            type: 'licenses',
+            attributes: {
+              key
+            },
+            relationships: {
+              policy: {
+                data: { type: 'policies', id: KEYGEN_POLICY_ID }
+              },
+              // If you're associating the license with a specific user, uncomment this line
+              // and provide their user ID:
+              // user: {
+              //   data: { type: 'users', id: userId }
+              // }
+            }
+          }
+        })
+      })
+
+      // Check if we received an error from the Keygen API
+      const { data, errors } = await res.json()
+      if (errors) {
+        const msg = errors.map(e => `${res.status} ${e.title}: ${e.detail}`).join('\n')
+
+        throw new Error(`Failed to generate license!\n${msg}`)
+      }
+
+      console.log(chalk.green(`License key successfully generated!`))
+      console.log(chalk.gray(`Signed key: ${data.attributes.key}`))
+
+      break
+    }
+    case 'verify': {
+      let key = null
+
+      // Parse argument flags
+      argv.forEach((arg, i, argv) => {
+        switch (arg) {
+          case '--key':
+          case '-k':
+            key = argv[i + 1]
+            break
+        }
+      })
+
+      if (!key) {
+        throw new Error(`License key is missing!`)
+      }
+
+      // Extract key and signature from the license key string
+      const [enc, sig] = key.split('.')
+
+      // Decode the base64 encoded key
+      const dec = Buffer.from(enc, 'base64').toString()
+
+      // Verify the signature of the key
+      const verifier = crypto.createVerify('sha256')
+      verifier.write(dec)
+      verifier.end()
+
+      const ok = verifier.verify({ key: KEYGEN_PUBLIC_KEY, padding: crypto.constants.RSA_PKCS1_PSS_PADDING }, sig, 'base64')
+      if (ok) {
+        console.log(chalk.green(`License key is cryptographically valid!`))
+        console.log(chalk.gray(`Embedded data: ${dec}`))
+      } else {
+        console.error(chalk.red('License key is not valid!'))
+      }
+
+      break
+    }
+    default:
+      throw new Error(`Invalid command: ${cmd}`)
+  }
+}
+
+main().catch(err =>
+  console.error(
+    chalk.red(`Error! ${err.message}`)
+  )
+)
